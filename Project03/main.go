df133f390e2b02255cd6f6b082a2be1b15fb9f67
rf <rf@ryans-MacBook-Air.local>
2024-05-26 11:32:45 +1000

    project 03 love o meter with 20 degree baseline

--- Project03/main.go
+++ Project03/main.go
@@ -7,8 +7,11 @@ import (
 )
 
 const (
-	sensorPin    float64 = 0
-	baseLineTemp float64 = 20.0
+	baseLineTemp = 20.0
+	increment    = 2.0
+	tempLow      = baseLineTemp + increment
+	tempMid      = tempLow + increment
+	tempHigh     = tempMid + increment
 )
 
 func main() {
@@ -31,9 +34,25 @@ func main() {
 		// ADC is read in using 16-bit resolution, so the value will be between 0 and 65535.
 		// This will be a fraction of the input voltage (0-5V), depending on the voltage divider.
 		// To get the voltage, we divide the ADC value by the maximum value of unsigned 16-bit ADC (65535) and multiply by 5V
-		voltage := float64(A0.Get()) / float64(math.MaxUint16) * 5
-		temperatureCelcius := (voltage - 0.5) * 100
+		volts := 5.0
+		sensorVoltage := float64(A0.Get()) / float64(math.MaxUint16) * volts
+		temperatureCelcius := (sensorVoltage - 0.5) * 100
 		println(int(temperatureCelcius)) // truncate degree
+
+		if temperatureCelcius >= tempLow && temperatureCelcius < tempMid {
+			pin2.High()
+			pin3.Low()
+			pin4.Low()
+		} else if temperatureCelcius >= tempMid && temperatureCelcius < tempHigh {
+			pin2.High()
+			pin3.High()
+			pin4.Low()
+		} else if temperatureCelcius >= tempHigh {
+			pin2.High()
+			pin3.High()
+			pin4.High()
+		}
+
 		time.Sleep(time.Second)
 	}
 }
