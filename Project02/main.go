c67fc2d6cf7bf582194cc36b5742e849067e44a5
rf <rf@ryans-MacBook-Air.local>
2024-05-26 10:58:34 +1000

    added in project02

---
+++ Project02/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"machine"
+	"time"
+)
+
+func main() {
+	pin3 := machine.D3
+	pin3.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	pin4 := machine.D4
+	pin4.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	pin5 := machine.D5
+	pin5.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	pin2 := machine.D2
+	pin2.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	for {
+		switchStateIsOn := pin2.Get()
+
+		if switchStateIsOn {
+			pin3.Set(false)
+			pin4.Set(false)
+			pin5.Set(true)
+
+			time.Sleep(time.Second)
+
+			pin4.Set(true)
+			pin5.Set(false)
+
+			time.Sleep(time.Second)
+		} else {
+			pin3.Set(true)
+			pin4.Set(false)
+			pin5.Set(false)
+		}
+	}
+}
